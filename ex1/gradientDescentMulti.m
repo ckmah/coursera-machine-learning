8fadc63471aeaf6b8be7fc3490a0167ad1d018be
Clarence Mah <clarence.iyan@gmail.com>
2016-03-26 22:57:09 -0700

    multiple linear regression

--- ex1/gradientDescentMulti.m
+++ ex1/gradientDescentMulti.m
@@ -6,30 +6,23 @@ function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters
 % Initialize some useful values
 m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
-
 for iter = 1:num_iters
+    h = (X * theta);
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
     %
-
-
-
-
-
-
-
-
-
-
+    for i = 1:size(X, 2)
+      theta(i) = theta(i) - sum((h - y) .* X(:, i)) * alpha / m;
+    end
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end
